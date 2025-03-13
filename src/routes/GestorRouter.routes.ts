de7c49e292d980dc009d74cf77566909ea03aabb
PaulPPS632 <jeffertsap@hotmail.com>
2025-03-13 17:41:37 -0500

    cambios xd

--- src/routes/GestorRouter.routes.ts
+++ src/routes/GestorRouter.routes.ts
@@ -5,9 +5,11 @@ import { upload } from "../middlewares/upload";
 const GestorRouter = Router();
 const gestorController = new GestorController();
 
-GestorRouter.get("/listObjects", gestorController.listObjects);
-GestorRouter.get("/listObjectsv2", gestorController.listObjectsv2);
+//GestorRouter.get("/listObjects", gestorController.listObjects);
+GestorRouter.get("", gestorController.listObjectsv2);
 GestorRouter.post("/upload", upload.array("files"), gestorController.upload);
 GestorRouter.delete("",gestorController.delete);
 GestorRouter.put("", gestorController.update);
+GestorRouter.get("/search", gestorController.search);
+
 export default GestorRouter;
